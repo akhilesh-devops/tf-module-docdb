04b9aea8cd46cbc7454837abd34dab9142525e14
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-19 23:14:59 +0530

    test

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ resource "aws_docdb_subnet_group" "main" {
   name       = "${local.name_prefix}-subnet-group"
   subnet_ids = var.subnet_ids
 
-  tags = {
-    Name = merge(local.tags, { Name = "${local.name_prefix}-subnet-group" })
-  }
+  tags = merge(local.tags, { Name = "${local.name_prefix}-subnet-group" })
+
 }

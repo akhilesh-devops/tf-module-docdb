7d8bd9fceca8b25315a7f8d4511eb7ff10d7948d
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-20 00:35:58 +0530

    test

--- variables.tf
+++ variables.tf
@@ -4,3 +4,5 @@ variable "tags" {}
 variable "backup_retention_period" {}
 variable "preferred_backup_window" {}
 variable "skip_final_snapshot" {}
+variable "sg_ingress_cidr" {}
+variable "vpc_id" {}
